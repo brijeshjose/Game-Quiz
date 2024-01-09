eb056188fb64c991ecce8c8635289f51bf7929f7
brijeshjose <brijeshjose921@gmail.com>
2024-01-09 23:07:40 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -77,6 +77,270 @@ function App() {
         },
       ],
     },
+    {
+      id: 4,
+      question: "What is the pH value of the human body?",
+      answers: [
+        {
+          text: "9.2 to 9.8",
+          correct: false,
+        },
+        {
+          text: "7.2 to 8.2",
+          correct: false,
+        },
+        {
+          text: "5.2 to 6.8",
+          correct: false,
+        },
+        {
+          text: "7.0 to 7.8",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 5,
+      question: " Which of the following are called Key Industrial animals?",
+      answers: [
+        {
+          text: "Producers",
+          correct: false,
+        },
+        {
+          text: "Tertiary consumers",
+          correct: false,
+        },
+        {
+          text: "Primary consumers",
+          correct: true,
+        },
+        {
+          text: "None of these",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 6,
+      question: " Which of the given amendments made it compulsory for the president to consent to the constitutional Amendment bills?",
+      answers: [
+        {
+          text: "27th",
+          correct: false,
+        },
+        {
+          text: "24th",
+          correct: true,
+        },
+        {
+          text: "26th",
+          correct: false,
+        },
+        {
+          text: "29th",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 7,
+      question: "Which of the following Himalayan regions is called Shivalik's?",
+      answers: [
+        {
+          text: "Upper Himalayas",
+          correct: false,
+        },
+        {
+          text: "Lower Himalayas",
+          correct: false,
+        },
+        {
+          text: "Outer Himalayas",
+          correct: true,
+        },
+        {
+          text: "Inner Himalayas",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 8,
+      question: "The Samkhya School of Philosophy was founded by?",
+      answers: [
+        {
+          text: "Gautam Buddha",
+          correct: false,
+        },
+        {
+          text: "Mahipala",
+          correct: false,
+        },
+        {
+          text: "Gopala",
+          correct: false,
+        },
+        {
+          text: "Kapila",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 9,
+      question: "Pustaz grasslands are situated at?",
+      answers: [
+        {
+          text: "South Africa",
+          correct: false,
+        },
+        {
+          text: "China",
+          correct: false,
+        },
+        {
+          text: "Hungary",
+          correct: true,
+        },
+        {
+          text: "USA",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 10,
+      question: "Chelaiya Samiti is related to which of the following?",
+      answers: [
+        {
+          text: "Banking sector",
+          correct: false,
+        },
+        {
+          text: "Health Sector",
+          correct: false,
+        },
+        {
+          text: "Tax reforms",
+          correct: true,
+        },
+        {
+          text: "Insurance sector",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 11,
+      question: "Which of the given devices is used for counting blood cells?",
+      answers: [
+        {
+          text: "Hmelethometer",
+          correct: false,
+        },
+        {
+          text: "Spyscometer",
+          correct: false,
+        },
+        {
+          text: "Hemocytometer",
+          correct: true,
+        },
+        {
+          text: "Hamosytometer",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 12,
+      question: "Which of the given compounds is used to make fireproof clothing?",
+      answers: [
+        {
+          text: "Aluminum chloride",
+          correct: false,
+        },
+        {
+          text: "Aluminum Sulphate",
+          correct: true,
+        },
+        {
+          text: "Magnesium Chloride",
+          correct: false,
+        },
+        {
+          text: "Magnesium Sulphate",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 13,
+      question: "Which of the given is a disease caused by protozoa?",
+      answers: [
+        {
+          text: "Cancer",
+          correct: false,
+        },
+        {
+          text: "Typhoid",
+          correct: false,
+        },
+        {
+          text: "Kala-azar",
+          correct: true,
+        },
+        {
+          text: "Chicken Pox",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 14,
+      question: "Which of the given coins was known as 'Karshapana' in ancient literature?",
+      answers: [
+        {
+          text: "Gold coins",
+          correct: false,
+        },
+        {
+          text: "Bronze coins",
+          correct: false,
+        },
+        {
+          text: "Punch marked coins",
+          correct: true,
+        },
+        {
+          text: "Iron coins",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 15,
+      question: "Rashtriya Swasthya Bima Yojana Launched under Social Security Act 2008 Involves?",
+      answers: [
+        {
+          text: "Only rural workers",
+          correct: false,
+        },
+        {
+          text: "Only social workers",
+          correct: false,
+        },
+        {
+          text: "Only Unorganized sector workers",
+          correct: true,
+        },
+        {
+          text: "All of the above",
+          correct: false,
+        },
+      ],
+    },
   ];
 
   const moneyPyramid = useMemo(
